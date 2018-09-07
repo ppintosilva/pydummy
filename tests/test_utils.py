234a4aeba1ba033cb7628931b59795c450629865
PedrosWits <ppintodasilva@gmail.com>
2018-09-07 17:02:33 +0100

    Refactored using pytest fixtures and fixed assert bug on py27

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,8 @@
+import io
 import os
 import ast
 import json
+import shutil
 import hashlib
 import requests
 import logging as lg
@@ -11,65 +13,79 @@ import pytest
 
 ###
 
-def assert_file_structure(assert_logfile = True):
+@pytest.fixture
+def destroy_folders():
     app_folder = dummy.settings["app_folder"]
 
+    if os.path.exists(app_folder):
+        shutil.rmtree(app_folder)
+
+    assert not os.path.exists(app_folder)
+
+###
+
+def assert_file_structure():
+    app_folder = dummy.settings["app_folder"]
     logs_folder = os.path.join(app_folder, dummy.settings["logs_folder_name"])
     data_folder = os.path.join(app_folder, dummy.settings["data_folder_name"])
     cache_folder = os.path.join(app_folder, dummy.settings["cache_folder_name"])
-
-    if assert_logfile:
-        log_filename = os.path.join(app_folder, dummy.settings["logs_folder_name"], '{}_{}.log'.format(dummy.settings["app_name"], dt.datetime.today().strftime('%Y_%m_%d')))
-        assert os.path.exists(log_filename)
-        os.remove(log_filename)
-        assert not os.path.exists(log_filename)
 
     assert os.path.exists(app_folder)
     assert os.path.exists(logs_folder)
     assert os.path.exists(data_folder)
     assert os.path.exists(cache_folder)
 
-    os.rmdir(logs_folder)
-    os.rmdir(data_folder)
-    os.rmdir(cache_folder)
-    os.rmdir(app_folder)
+def assert_log_file():
+    log_filename = os.path.join(dummy.settings["app_folder"], dummy.settings["logs_folder_name"], '{}_{}.log'.format(dummy.settings["app_name"], dt.datetime.today().strftime('%Y_%m_%d')))
 
-    assert not os.path.exists(app_folder)
-    assert not os.path.exists(logs_folder)
-    assert not os.path.exists(data_folder)
-    assert not os.path.exists(cache_folder)
+    assert os.path.exists(log_filename)
 
 ###
 
 def test_get_app_folder():
     dummy.settings["app_folder"] == os.path.expanduser("~/.pydummy")
 
-def test_create_folders():
+###
+
+def test_create_folders(destroy_folders):
     dummy.create_folders()
-    assert_file_structure(assert_logfile = False)
+    assert_file_structure()
+    destroy_folders
+
+###
 
 def test_config_immutable_setting():
     with pytest.raises(dummy.ImmutableSetting):
         dummy.config(app_name = "test")
 
+###
+
 def test_config_invalid_setting():
     with pytest.raises(dummy.InvalidSetting):
         dummy.config(verbose = True)
 
-def test_config():
+###
+
+def test_config(destroy_folders):
     dummy.config(log_to_console = True)
 
     assert dummy.settings["log_to_console"] == True
     assert dummy.settings["cache_http"] == True
     assert dummy.settings["log_default_level"] == lg.INFO
 
-    assert_file_structure(assert_logfile = True)
+    assert_file_structure()
+    assert_log_file()
+    destroy_folders
 
     dummy.config(app_folder = "/tmp/pydummy")
 
-    assert_file_structure(assert_logfile = True)
+    assert_file_structure()
+    assert_log_file()
+    destroy_folders
 
-def test_cache():
+###
+
+def test_cache(destroy_folders):
     dummy.config(cache_http = True)
 
     url = requests.Request('GET', "https://nominatim.openstreetmap.org/?format=json&addressdetails=1&q=Newcastle+A186+Westgate+Rd").prepare().url
@@ -83,15 +99,18 @@ def test_cache():
 
     assert os.path.exists(cache_file)
 
-    with open(cache_file, encoding='utf-8') as cache_file_handler:
-        cache_content = json.loads(cache_file_handler.read())
+    with io.open(cache_file, 'r', encoding='utf-8') as cache_file_handler:
+        cache_content = json.load(cache_file_handler)
 
-    json_str = dummy.make_str(json.dumps(response_json))
+    json_str = json.dumps(response_json)
     json_str_from_cache = dummy.get_from_cache(url)
 
-    assert ast.literal_eval(json_str) == cache_content
+    try:
+        # This fails in python2 for awkward reasons
+        assert json_str == json.dumps(cache_content)
+    except:
+        assert response_json == json_str_from_cache
+
     assert cache_content == json_str_from_cache
 
-    os.remove(cache_file)
-
-    assert_file_structure(assert_logfile = False)
+    destroy_folders
